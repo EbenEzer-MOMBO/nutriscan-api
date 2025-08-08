6a3bb9e8c6fdac82cd1c85c22081f09ba5f2e840
MOMBO Eben <ebenezermombo@gmail.com>
2025-08-08 18:07:58 +0100

    Update

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.send('Hell!')
 })
 
- app.listen(port, function () {
+app.listen(port, function () {
   console.log(`Example app started! on port ${port}`)
 })
